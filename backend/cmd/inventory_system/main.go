6a8b6f3bd060921504a74976f331c6733696e340
Demeyer10 <04_zoster_borscht@icloud.com>
2025-04-08 22:38:34 -0400

    Adding jwt authentciation

--- backend/cmd/inventory_system/main.go
+++ backend/cmd/inventory_system/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"github.com/LostProgrammer1010/Inventory-System/internal/db"
-	"github.com/LostProgrammer1010/Inventory-System/internal/server"
+	"fmt"
+
+	"github.com/LostProgrammer1010/InventorySystem/internal/db"
+	"github.com/LostProgrammer1010/InventorySystem/internal/server"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
-
-	db.Connect()
-
+	godotenv.Load()
+	db.Init()
+	id, _ := primitive.ObjectIDFromHex("67f56f3cff37aeb01063beba")
+	user, err := db.GetUser(id)
+	fmt.Println(user, err)
 	server.Start()
 }
